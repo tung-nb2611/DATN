384c6c9bc493599c3452ad0f6d6165f04bf5827b
tungnb <100818231+tung-nb2611@users.noreply.github.com>
2023-10-11 09:00:18 +0700

    Update StartWebApplication.java

--- back-end/com.sapo.repair-motobike-shop/src/main/java/com/sapo/StartWebApplication.java
+++ back-end/com.sapo.repair-motobike-shop/src/main/java/com/sapo/StartWebApplication.java
@@ -12,6 +12,6 @@ import java.util.Random;
 @SpringBootApplication
 public class StartWebApplication {
     public static void main(String[] args){
-        SpringApplication.run(StartWebApplication.class, args);
+        // SpringApplication.run(StartWebApplication.class, args);
     }
 }
